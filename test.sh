a45a106a12f0e30980e8229cf7d77c67f6e6a805
Simon Evans <si@si.org>
2021-06-25 23:49:12 +0100

    build: Adding testing on swift 5.2
    - Downgrade minimum requirement from 5.3 to 5.2 to check coverage
      on older versions.
    
    - Add a Dockerfile for 5.2 and change the testing from the nightly
      to the latest released version. This covers testing on both 5.2
      and latest versions.
    
    - Add a .dockerfileignore to reduce the files copied into the docker
      container.

--- test.sh
+++ test.sh
@@ -2,11 +2,15 @@
 
 MACOS_TESTS="Passed"
 LINUX_TESTS="Passed"
+LINUX_5_2_TESTS="Passed"
 
 swift package clean
 swift test || MACOS_TESTS="Failed"
-docker build --tag=swift-babab-tests:$(date +%s) . || LINUX_TESTS="Failed"
+
+docker build -f Dockerfile-5.2 --build-arg CACHEBUST=$(date +%s) --tag=swift-babab-tests-5-2:$(date +%s) . || LINUX_5_2_TESTS="Failed"
+docker build -f Dockerfile --build-arg CACHEBUST2=$(date +%s) --tag=swift-babab-tests:$(date +%s) . || LINUX_TESTS="Failed"
 
 echo
 echo macOS tests ${MACOS_TESTS}
+echo Linux-5.2 tests ${LINUX_5_2_TESTS}
 echo Linux tests ${LINUX_TESTS}
